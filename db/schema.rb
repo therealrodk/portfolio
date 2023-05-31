a4318e5815a0e2cae9d53ab1e0485ce46fecafce
R K <armyofbuddhas@gmail.com>
2023-05-31 15:59:29 -0600

    Add published at for draft, published, and scheduled Blog Posts.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_31_161726) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_31_210206) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -56,6 +56,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_31_161726) do
     t.string "title", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "published_at"
   end
 
   create_table "users", force: :cascade do |t|
